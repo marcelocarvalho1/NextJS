13ebc233b4230e541f79b30e3fc955ee51210d7f
Marcelo <marceloo.dev@gmail.com>
2025-07-11 21:26:46 -0300

    Input and handleSearchUser

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,3 @@
-import { Button } from "@/components/button";
-
 interface PostProps {
   id: number;
   title: string;
@@ -15,15 +13,48 @@ export default async function PostsPage() {
   const response = await fetch("https://dummyjson.com/posts");
   const data: ResponseProps = await response.json();
 
-  console.log(data);
+  async function handleFetchPosts() {
+    "use server";
+    const response = await fetch("https://dummyjson.com/posts");
+    const data: ResponseProps = await response.json();
+
+    console.log(data.posts);
+  }
+  async function handlerSearchUsers(formData: FormData) {
+    "use server";
+
+    const userID = formData.get("userID");
+
+
+    const response = await fetch(`https://dummyjson.com/posts/user/${userID} `);
+    const data: ResponseProps = await response.json();
+
+
+    console.log(data);
+  }
 
   return (
     <div>
-      <h1 className="text-center mt-5 mb-3 font-bold text-2xl">Todos os Posts</h1>
+      <h1 className="text-center mt-5 mb-3 font-bold text-2xl">
+        Todos os Posts
+      </h1>
 
-      <Button/>
+      <form className="flex gap-2 my-4" action={handlerSearchUsers}>
+        <input
+          type="text"
+          placeholder="Id do usuário"
+          className="border border-gray-200 p-1"
+          name="userID"
+        />
 
-
+        <button
+          className="bg-blue-500 text-white p-1 rounded-sm"
+          type="submit"
+          onClick={handleFetchPosts}
+        >
+          Buscar posts
+        </button>
+      </form>
 
       <div className="flex flex-col gap-4 mx-2">
         {data.posts.map((post) => (
