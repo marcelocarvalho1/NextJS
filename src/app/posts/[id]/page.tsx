68452f809f8968e86b4b4e7328a554b9aaf8b020
Marcelo <marceloo.dev@gmail.com>
2025-07-16 23:51:30 -0300

    routes id

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,19 @@
+export default async function DetailPost({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+  
+}) {
+  const { id } = await params;
+
+   const response = await fetch(`https://dummyjson.com/posts/${id} `);
+    const data: Postrops = await response.json();
+
+  return (
+    <div>
+      <h1> Detalhes do post: {id} </h1>
+      <h2> {data.title}</h2>
+      <p> {data.body} </p>
+    </div>
+  );
+}
