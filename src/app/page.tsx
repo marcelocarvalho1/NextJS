02eb4cf3514f7c43e146a6ba6e252fc443dfc8a4
Marcelo <marceloo.dev@gmail.com>
2025-07-22 00:21:05 -0300

    revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Home - teste',
-  description: 'NextJS',
-  openGraph:{
-    title: 'NextJS Loren',
-    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus minus nostrum est temporibus laudantium.',
-  }
-}
+  title: "Home - teste",
+  description: "NextJS",
+  openGraph: {
+    title: "NextJS Loren",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus minus nostrum est temporibus laudantium.",
+  },
+};
+
+export const revalidate = 10
 
 export default function Home() {
+  const randomNumber = Math.random() * 10;
   return (
     <div>
       <h1> Página HOME</h1>
+      <h2> Numero gerado: {randomNumber}</h2>
     </div>
   );
 }
